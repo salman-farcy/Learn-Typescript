26aa4c51b3f5f2c1b69bf29ae03b5b99253714e3
salman-farcy <salmanfarcy253@gmail.com>
2024-03-21 18:26:05 +0600

    little overview of how typescript is used

--- Ts/index.ts
+++ Ts/index.ts
@@ -1,5 +1,16 @@
-console.log('I am slaman farcy technical');
-let num:number = 5
-num = 55
-
+// console.log('I am slaman farcy technical');
+// let num:number = 5
+// num = 55
 // num = 'salman' //!this is not valide in typescript
+
+//TODO: 1: Write a program to add  two numbers ? 
+//TODO: 2: How to catch Errors & solve it ?
+//TODO: 3: Ts Configuration File
+
+// @ts-ignore
+//* func derine 
+function sum(a:number, b:number):number{
+     return a + b
+}
+
+console.log(sum(5, 10));
