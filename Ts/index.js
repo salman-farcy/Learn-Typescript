9baf90d8709c308f75324040eb5720532288c97a
salman-farcy <salmanfarcy253@gmail.com>
2024-03-21 14:53:42 +0600

    TypeScript setup complete

---
+++ Ts/index.js
@@ -0,0 +1,4 @@
+console.log('I am slaman farcy technical');
+var num = 5;
+num = 55;
+// num = 'salman' //!this is not valide in typescript
--- Ts/index.ts
+++ Ts/index.ts
@@ -1,3 +1,5 @@
 console.log('I am slaman farcy technical');
 let num:number = 5
-num = 'salman' //!this is not valide in typescript
+num = 55
+
+// num = 'salman' //!this is not valide in typescript
