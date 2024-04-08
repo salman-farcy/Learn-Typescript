59b138227deef2757b8a1dbbc537a18640a708bb
salman-farcy <salmanfarcy253@gmail.com>
2024-04-08 20:20:24 +0600

    some typescript code

--- Ts/types/type_annotation.js
+++ Ts/types/type_annotation.js
@@ -1,6 +1,35 @@
-var myFavNum = 10;
-var myFullName = "salman farcy is the great parsone";
-// myFavNum = "thapa";
-console.log(myFavNum.toString());
-var sum1 = myFavNum + 10;
-console.log(sum1);
+// let myFavNum:number = 10;
+// let myFullName:string = "salman farcy is the great parsone";
+// // myFavNum = "thapa";
+// console.log(myFavNum.toString());
+// let sum1 = myFavNum + 10;
+// console.log(sum1);
+var myFullName = "salman farcy";
+var myFirstName = "salman";
+var myLastName = "farcy";
+myFullName = myFirstName + myLastName;
+// console.log(myFullName)
+// TODO: string initialization: 
+var message = "Hello , Typescript!";
+// console.log(message)
+// TODO: string Length: 
+var sentence = "Please subscribe my channel";
+var sentenceLength = sentence.length;
+// console.log(sentenceLength);
+//  TODO: Uppercase and Lowercase:
+var text = "Please Subscribe My Channel";
+var uppercaseText = text.toUpperCase();
+var lowercaseText = text.toLowerCase();
+// console.log(uppercaseText);
+// console.log(lowercaseText);
+// TODO: BOOLEAN TYPE
+var isMyNameVinod = true;
+var isDone = true;
+var hasStarted = false;
+// hasStarted = "yes" // invalid
+//!Question: Check Even Number
+function isEven(a) {
+    return a % 2 == 0;
+}
+//functiion call
+console.log(isEven(11));
--- Ts/types/type_annotation.ts
+++ Ts/types/type_annotation.ts
@@ -1,11 +1,57 @@
 
 
-let myFavNum:number = 10;
-let myFullName:string = "salman farcy is the great parsone";
+// let myFavNum:number = 10;
+// let myFullName:string = "salman farcy is the great parsone";
 
 
-// myFavNum = "thapa";
-console.log(myFavNum.toString());
+// // myFavNum = "thapa";
+// console.log(myFavNum.toString());
 
-let sum1 = myFavNum + 10;
-console.log(sum1);
+// let sum1 = myFavNum + 10;
+// console.log(sum1);
+
+
+let myFullName = "salman farcy"
+let myFirstName = "salman"
+let myLastName = "farcy"
+
+myFullName = myFirstName + myLastName
+// console.log(myFullName)
+
+
+// TODO: string initialization: 
+let message:string = "Hello , Typescript!"
+// console.log(message)
+
+
+
+// TODO: string Length: 
+let sentence:string =  "Please subscribe my channel";
+let sentenceLength:number = sentence.length;
+// console.log(sentenceLength);
+
+
+//  TODO: Uppercase and Lowercase:
+let text:string = "Please Subscribe My Channel";
+let uppercaseText:string = text.toUpperCase();
+let lowercaseText:string = text.toLowerCase();
+// console.log(uppercaseText);
+// console.log(lowercaseText);
+
+
+// TODO: BOOLEAN TYPE
+let isMyNameVinod:boolean = true;
+let isDone:boolean = true;
+let hasStarted:boolean = false;
+
+
+// hasStarted = "yes" // invalid
+
+
+//!Question: Check Even Number
+function isEven(a:number):boolean{
+     return a % 2 ==  0
+}
+
+//functiion call
+console.log(isEven(11));
